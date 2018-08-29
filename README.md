56461ce790fe0853d623fb5b684302f7feaf225e
Leonardo_zyh <956731563@qq.com>
2018-08-29 19:18:46 +0800

    完成Markdown部分

---
+++ README.md
@@ -0,0 +1,27 @@
+
+#异步回调
+/*函数中会设置可执行函数调用的参数*/
+function writeCode(fn){
+    fn.call()
+}
+
+
+writeCode(result,
+    function(){                    
+        creatPaper(`()=>{}`)
+    })
+
+/*writeCode callback the function*/
+//异步：【不等结果】直接进行下一步
+//回调：拿到异步的一种方式
+
+//异步，定闹钟：10秒后开始执行第一行代码
+//定闹钟；writeCode返回；执行fn()；时间到执行代码
+
+var promise = $.get('/xxx')    /*1s后拿到结果，不等你，直到执行下一步*/
+promise.then(success,error)
+
+
+#注意：
+设置overflow: hidden才能使用页面滑动
+domCode.scrollTop = domCode.scrollHeight
--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+/*把code写到#code和style标签里*/
+function writeCode(prefix,code,fn ) {
+    let domCode = document.querySelector('#code')
+    //domCode.innerHTML = prefix || ''
+    let n = 0
+    let id = setInterval(() => {
+        n += 1 
+        domCode.innerHTML =  Prism.highlight(prefix + code.substring(0, n), Prism.languages.css)
+        styleTag.innerHTML = prefix + code.substring(0, n)
+        domCode.scrollTop = domCode.scrollHeight
+        if (n >= code.length) {
+            window.clearInterval(id)
+            fn.call()
+
+        }
+    }, 0)
+}
+
+function writeMarkdown(markdown,fn){
+    let domPaper = document.querySelector('.content')
+    let n = 0
+    let id = setInterval(() => {
+        n += 1 
+        domPaper.innerHTML =  markdown.substring(0, n)
+        domPaper.scrollTop = 10000//domPaper.scrollHeight
+        //console.log(domPaper)
+        if (n >= markdown.length) {
+            window.clearInterval(id)
+            fn.call()
+        }
+    }, 100)
+}
+
+
 var result = `
 /* 
  * 面试官你好，我是XXX
@@ -10,10 +44,11 @@ var result = `
     transition: all 1s;
   }
   html{
-    background: #eee;
+    background: rgb(222,222,222);
+    font-size:16px;
   }
   #code{
-    border: 1px solid #aaa;
+    border: 1px solid red;
     padding: 16px;
   }
 
@@ -35,44 +70,93 @@ var result = `
 
 /* 我来介绍一下我自己吧 */
 /* 我需要一张白纸 */
+#code{
+    position: fixed;
+    left: 0;
+    width: 50%;
+    height: 100%;
+}
+#paper{
+    position: fixed;
+    right: 0;
+    width: 50%;
+    height: 100%;
+    background:grey;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    padding:6px;
+
+}
+#paper > .content{
+    background:white; 
+    width:100%;
+    height:100%;
+    padding-left:16px;
+   
+}
+`
+var result2 = `
+    #parper{
+
+    }
+    /*
+    接下来把markdown变成HTML - marked.js
+    */
+   /*
+   这就是我的会动的简历
+   谢谢观看！
+   */
+    `
+var md = `
+# 自我介绍
+我叫曾宇豪
+1997年3月出生
+XXX学校毕业
+自学前端半年
+希望应聘前端开发岗位
+
+＃技能介绍
+
+熟悉JavaScript CSS
+
+＃项目介绍
+
+1. XXX轮播
+2. XXX简历
+3. XXX画板
+
+＃联系方式
+
+-  QQ：956731563
+- 电子邮件：956731563@qq.com
+- 手机:15215210604
+
 `
 
 
+writeCode('',result,()=>{    //writeCode callback the function
+    creatPaper(()=>{
+        writeCode(result,result2,()=>{
+            writeMarkdown(md,()=>{
+                markdownToHTML(()=>{
+                    write(result+result2,result3)
+                })
+            })
+        })
+    })
+})//定闹钟：10秒后开始执行第一行代码
+//异步：【不等结果】直接进行下一步
+//回调：拿到异步的一种方式
+//定闹钟；writeCode返回；执行fn()；时间到执行代码
 
-var n = 0
-var id = setInterval(() => {
-    n += 1
-    code.innerHTML = result.substring(0, n)
-    code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css)
-    styleTag.innerHTML = result.substring(0, n)
-    if (n >= result.length) {
-        window.clearInterval(id)
-        fn2()
-        fn3(result)
-    }
-}, 10)
-
-function fn2() {
+function creatPaper(fn) {
     var paper = document.createElement('div')
-    paper.id = 'paper'
+    paper.id = 'paper'    
+    var content = document.createElement('pre')
+    content.className = 'content'
+    paper.appendChild(content)
     document.body.appendChild(paper)
-}
+    fn.call()
 
-function fn3(preResult) {
-    var result = `
-    #paper{
-        width:100px;height:100px;
-        background:red
-    }
-    `
-    var n = 0
-    var id = setInterval(() => {
-        n += 1
-        code.innerHTML = preResult + result.substring(0, n)
-        code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css)
-        styleTag.innerHTML = preResult + result.substring(0, n)
-        if (n >= result.length) {
-            window.clearInterval(id)
-        }
-    },10)
 }
